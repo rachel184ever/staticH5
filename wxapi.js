b1540b377a4e3bb5cca91e5e9058ba5e7062d351
王玉静 <wangyujing1@ZBMAC-C02WD3R2H.local>
2018-11-14 17:43:14 +0800

    wx api

---
+++ wxapi.js
@@ -0,0 +1,19 @@
+function ready() {
+    console.log(window.__wxjs_environment === 'miniprogram') // true
+    wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success(res) {
+            // tempFilePath可以作为img标签的src属性显示图片
+            const tempFilePaths = res.tempFilePaths
+            console.log(tempFilePaths)
+        }
+    })
+}
+
+if (!window.WeixinJSBridge || !WeixinJSBridge.invoke) {
+    document.addEventListener('WeixinJSBridgeReady', ready, false)
+} else {
+    ready()
+}
