836c6bcfab8c73a99a85e0e48c97c50c38a3e8f8
王玉静 <wangyujing1@ZBMAC-C02WD3R2H.local>
2018-11-16 09:58:10 +0800

    test\

--- wxapi.js
+++ wxapi.js
@@ -1,75 +1,75 @@
-function add(x, y) {  
-    return((x & 0x7FFFFFFF) + (y & 0x7FFFFFFF)) ^ (x & 0x80000000) ^ (y & 0x80000000);  
-}  
+function add(x, y) {
+    return ((x & 0x7FFFFFFF) + (y & 0x7FFFFFFF)) ^ (x & 0x80000000) ^ (y & 0x80000000);
+}
 
-function SHA1hex(num) {  
-    var sHEXChars = "0123456789abcdef";  
-    var str = "";  
-    for(var j = 7; j >= 0; j--)  
-        str += sHEXChars.charAt((num >> (j * 4)) & 0x0F);  
-    return str;  
-}  
+function SHA1hex(num) {
+    var sHEXChars = "0123456789abcdef";
+    var str = "";
+    for (var j = 7; j >= 0; j--)
+        str += sHEXChars.charAt((num >> (j * 4)) & 0x0F);
+    return str;
+}
 
-function AlignSHA1(sIn) {  
-    var nblk = ((sIn.length + 8) >> 6) + 1,  
-        blks = new Array(nblk * 16);  
-    for(var i = 0; i < nblk * 16; i++) blks[i] = 0;  
-    for(i = 0; i < sIn.length; i++)  
-        blks[i >> 2] |= sIn.charCodeAt(i) << (24 - (i & 3) * 8);  
-    blks[i >> 2] |= 0x80 << (24 - (i & 3) * 8);  
-    blks[nblk * 16 - 1] = sIn.length * 8;  
-    return blks;  
-}  
+function AlignSHA1(sIn) {
+    var nblk = ((sIn.length + 8) >> 6) + 1,
+        blks = new Array(nblk * 16);
+    for (var i = 0; i < nblk * 16; i++) blks[i] = 0;
+    for (i = 0; i < sIn.length; i++)
+        blks[i >> 2] |= sIn.charCodeAt(i) << (24 - (i & 3) * 8);
+    blks[i >> 2] |= 0x80 << (24 - (i & 3) * 8);
+    blks[nblk * 16 - 1] = sIn.length * 8;
+    return blks;
+}
 
-function rol(num, cnt) {  
-    return(num << cnt) | (num >>> (32 - cnt));  
-}  
+function rol(num, cnt) {
+    return (num << cnt) | (num >>> (32 - cnt));
+}
 
-function ft(t, b, c, d) {  
-    if(t < 20) return(b & c) | ((~b) & d);  
-    if(t < 40) return b ^ c ^ d;  
-    if(t < 60) return(b & c) | (b & d) | (c & d);  
-    return b ^ c ^ d;  
-}  
+function ft(t, b, c, d) {
+    if (t < 20) return (b & c) | ((~b) & d);
+    if (t < 40) return b ^ c ^ d;
+    if (t < 60) return (b & c) | (b & d) | (c & d);
+    return b ^ c ^ d;
+}
 
-function kt(t) {  
-    return(t < 20) ? 1518500249 : (t < 40) ? 1859775393 :  
-        (t < 60) ? -1894007588 : -899497514;  
-}  
+function kt(t) {
+    return (t < 20) ? 1518500249 : (t < 40) ? 1859775393 :
+        (t < 60) ? -1894007588 : -899497514;
+}
 
-function SHA1(sIn) {  
-    var x = AlignSHA1(sIn);  
-    var w = new Array(80);  
-    var a = 1732584193;  
-    var b = -271733879;  
-    var c = -1732584194;  
-    var d = 271733878;  
-    var e = -1009589776;  
-    for(var i = 0; i < x.length; i += 16) {  
-        var olda = a;  
-        var oldb = b;  
-        var oldc = c;  
-        var oldd = d;  
-        var olde = e;  
-        for(var j = 0; j < 80; j++) {  
-            if(j < 16) w[j] = x[i + j];  
-            else w[j] = rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);  
-            t = add(add(rol(a, 5), ft(j, b, c, d)), add(add(e, w[j]), kt(j)));  
-            e = d;  
-            d = c;  
-            c = rol(b, 30);  
-            b = a;  
-            a = t;  
-        }  
-        a = add(a, olda);  
-        b = add(b, oldb);  
-        c = add(c, oldc);  
-        d = add(d, oldd);  
-        e = add(e, olde);  
-    }  
-    SHA1Value = SHA1hex(a) + SHA1hex(b) + SHA1hex(c) + SHA1hex(d) + SHA1hex(e);  
-    return SHA1Value.toUpperCase();  
-}  
+function SHA1(sIn) {
+    var x = AlignSHA1(sIn);
+    var w = new Array(80);
+    var a = 1732584193;
+    var b = -271733879;
+    var c = -1732584194;
+    var d = 271733878;
+    var e = -1009589776;
+    for (var i = 0; i < x.length; i += 16) {
+        var olda = a;
+        var oldb = b;
+        var oldc = c;
+        var oldd = d;
+        var olde = e;
+        for (var j = 0; j < 80; j++) {
+            if (j < 16) w[j] = x[i + j];
+            else w[j] = rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+            t = add(add(rol(a, 5), ft(j, b, c, d)), add(add(e, w[j]), kt(j)));
+            e = d;
+            d = c;
+            c = rol(b, 30);
+            b = a;
+            a = t;
+        }
+        a = add(a, olda);
+        b = add(b, oldb);
+        c = add(c, oldc);
+        d = add(d, oldd);
+        e = add(e, olde);
+    }
+    SHA1Value = SHA1hex(a) + SHA1hex(b) + SHA1hex(c) + SHA1hex(d) + SHA1hex(e);
+    return SHA1Value.toUpperCase();
+}
 
 const accToken = '15__NFeOk1iUOef8Ap2QidZLUu51fycxG4bV7yiYt0cwLnHuH7GI91IPXe1nzL8wjVPctZIMwbfDXP8xZ5ko3a4VTlGse3DMVESO55bMR1xOO59hdH1675IzDAOIzwocG_kwNRQnRMXuY0d2Ne2KOCaAAAPTH'
 const ticket = 'LIKLckvwlJT9cWIhEQTwfM8lwJeSbsPSo0DlFiwzpa5kK2KxAKCO9_w6aQEn56fQhtIC9IPGnT0XfZBkK253wQ'
@@ -125,14 +125,16 @@ function getUrlParam(name) {//封装方法
     var r = window.location.search.substr(1).match(reg); //匹配目标参数
     if (r != null) return unescape(r[2]);
     return null; //返回参数值
-    }
+}
 
-    console.log(getUrlParam("imgPath"))
+console.log(getUrlParam("imgPath"))
 wx.miniProgram.postMessage({ data: 'foo' })
-wx.miniProgram.postMessage({ data: {foo: 'bar'} })
-wx.miniProgram.getEnv(function(res) { console.log(res.miniprogram) })
-wx.chooseImage({
-            count: 1,
+wx.miniProgram.postMessage({ data: { foo: 'bar' } })
+wx.miniProgram.getEnv(function (res) { console.log(res.miniprogram) })
+
+function chooseImg() {
+    wx.chooseImage({
+        count: 1,
         sizeType: ['original', 'compressed'],
         sourceType: ['album', 'camera'],
         success(res) {
@@ -140,5 +142,17 @@ wx.chooseImage({
             const tempFilePaths = res.tempFilePaths
             console.log(tempFilePaths)
         }
-})
+    })
+
+}
+
+// web-view下的页面内
+function ready() {
+    chooseImg()
+  }
+  if (!window.WeixinJSBridge || !WeixinJSBridge.invoke) {
+    document.addEventListener('WeixinJSBridgeReady', ready, false)
+  } else {
+    ready()
+  }
 
